4a1f5c90997dec2e9c87104e191a741b73318b2e
Elizaveta K <elizaeliza1997@gmail.com>
2026-08-14 23:19:22 +0300

    Update2 fit_life.py

--- fit_life.py
+++ fit_life.py
@@ -1,4 +1,4 @@
-# Проект FitLife - MVP версия 1.0
+# Fit_Life project
 
 WATER_COAFF_UNDERWEIGHT = 0.028
 WATER_COAFF_NORMAL = 0.030
@@ -6,6 +6,7 @@ WATER_COAFF_OVERWEIGHT = 0.035
 
 
 def recommendations(bmi, weight):
+    '''Return the multiplication of two multupliers(bmi and weight)'''
     if bmi < 18.5:
         return weight * 0.028
     elif 18.5 <= bmi < 25:
@@ -14,10 +15,10 @@ def recommendations(bmi, weight):
         return weight * 0.035
 
 
-print('Привет!', 'Я виртуальный помощник от Fitlife!', sep=' ')
+print('Привет! Я виртуальный помощник от Fitlife!')
 print('Мне потребуются твои данные для расчета ИМТ.')
 name = input('Как я могу к тебе обращаться?')
-print(f'{name.capitalize()}, рад знакомству!')
+print(f'{name.title()}, рад знакомству!')
 user_age = int(input('Введи свой возраст:'))
 
 weight = float(input('Введи свой вес в кг:'))
@@ -25,6 +26,9 @@ height = float(input('Введи свой рост в метрах (пример
 bmi = weight / (height ** 2)
 print(f'Твой индекс массы тела (ИМТ): {round(bmi, 1)}')
 water_intake = recommendations(bmi, weight)
-
-print(f'Твоя норма суточного потребления воды: {water_intake} литров.')
+water_intake = (
+    f'Твоя норма суточного потребления воды:\n'
+    f'  {water_intake} литров.\n'
+)
+# print(f'')
 print('Следуй рекомендациям и будь здоров!')

37ca85553e787348b75506671579652e7b9eaa09
Елизавета Каменщикова <elizaeliza1997@gmail.com>
2026-08-16 13:09:48 +0300

    fix19

--- fit_life.py
+++ fit_life.py
@@ -2,7 +2,7 @@
 print('Привет!', 'Я виртуальный помощник от Fitlife!')
 print('Мне потребуются твои данные для расчета ИМТ.')
 name = input('Как я могу к тебе обращаться?')
-print('name.title()', ', рад знакомству!')
+print(f'{name.title()}, рад знакомству!')
 age = int(input('Введи свой возраст : '))
 weight = float(input('Введи свой вес в кг : '))
 height = float(input('Введи свой рост в м (пример - 1.55): '))
@@ -14,15 +14,15 @@ bmi = weight / (height ** 2)
 def water_intake_recommendations(bmi: float, weight: float) -> float:
     """Recommend daily water intake in liters based on BMI and weight."""
     if bmi < 18.5:
-        return weight * 0.030
+        return weight * 0.028
     elif 18.5 <= bmi < 25:
         return weight * 0.030
     else:
-        return weight * 0.030
+        return weight * 0.035
 
 
 water_intake = water_intake_recommendations(bmi, weight)
 
 
 print(f'Твой индекс массы тела (ИМТ): {bmi:.2f}')
-print(f'Твоя норма суточного потребления воды:{water_intake:.2f} литров.')
+print(f'Твоя норма воды в сутки:{water_intake:.2f} литров.')

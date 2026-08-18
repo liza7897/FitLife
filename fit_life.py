fff941aa94b35f69b42b000f7dc258a322909e8a
Елизавета Каменщикова <elizaeliza1997@gmail.com>
2026-08-18 12:25:13 +0300

    fix21

--- fit_life.py
+++ fit_life.py
@@ -1,29 +1,27 @@
-
-print('Привет!', 'Я виртуальный помощник от Fitlife!')
+print('Привет!', "Я виртуальный помощник от Fitlife!")
 print('Мне потребуются твои данные для расчета ИМТ.')
 name = input('Как я могу к тебе обращаться?')
-formatted_name = name.title()
-print(f'{formatted_name}, рад знакомству!')
-age = int(input('Введи свой возраст : '))
-weight = float(input('Введи свой вес в кг : '))
-height = float(input('Введи свой рост в м (пример - 1.55): '))
+print(name.title(), 'рад знакомству!')
+
+user_age = int(input('Введи свой возраст : '))
+user_weight = float(input('Введи свой вес в кг : '))
+user_height = float(input('Введи свой рост в м (пример - 1.55): '))
 
 """Расчет ИМТ"""
-bmi = weight / (height ** 2)
+bmi = user_weight / (user_height ** 2)
 
 
-def water_intake_recommendations(bmi: float, weight: float) -> float:
-    """Recommend daily water intake in liters based on BMI and weight."""
+def water_intake_recommendations(bmi):
+    """Функция расчета bmi"""
     if bmi < 18.5:
-        return weight * 0.028
+        return user_weight * 0.030
     elif 18.5 <= bmi < 25:
-        return weight * 0.030
+        return user_weight * 0.030
     else:
-        return weight * 0.035
+        return user_weight * 0.030
 
 
-water_intake = water_intake_recommendations(bmi, weight)
-
+water_intake = water_intake_recommendations(bmi)
 
 print(f'Твой индекс массы тела (ИМТ): {bmi:.2f}')
-print(f'Твоя норма воды в сутки:{water_intake:.2f} литров.')
+print(f'Твоя норма суточного потребления воды: {water_intake:.2f} литров.')
